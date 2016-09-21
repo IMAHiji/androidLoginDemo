bd11868ce9975b14d83ed285c68668ada5f07a7b
IMAHiji <lukeaskins@gmail.com>
2016-09-21 15:48:18 -0500

    Serialize user object on login to string and deserialize in useractivity

--- app/src/main/java/com/lukeaskins/logindemo/ui/LoginActivity.java
+++ app/src/main/java/com/lukeaskins/logindemo/ui/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,6 +14,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.lukeaskins.logindemo.R;
 import com.lukeaskins.logindemo.RegisterActivity;
 import com.lukeaskins.logindemo.lib.HHApiClient;
@@ -75,62 +77,72 @@ public class LoginActivity extends AppCompatActivity {
 
 
     public void login() {
-        Log.d(TAG, "LOGIN REQUESTED");
+        Log.v(TAG, "LOGIN REQUESTED");
+        //Check if Empty
+        if (isEmpty(mUsername) || isEmpty(mPassword)) {
+            //If either one is empty, Tell user to enter text
+            Toast.makeText(this, "Please enter your username and pasword", Toast.LENGTH_LONG).show();
+        } else {
+            Log.v(TAG, "LOGIN Begins");
+            String email = mUsername.getText().toString();
+            String password = mPassword.getText().toString();
 
+            //Check for network availability
+            if(isNetworkAvailable()){
+                //If available, Perform Login Request
 
-        Context context = this.getApplicationContext();
-        String email = mUsername.getText().toString();
-        String password = mPassword.getText().toString();
-        sessionEmail = mUsername.getText().toString();
-        sessionPassword = mPassword.getText().toString();
+                //Start building a retrofit object.
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl(URL_BASE)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
 
+                //Generate a login from the input data
+                UserLoginRequest userLoginRequest = new UserLoginRequest();
+                userLoginRequest.setEmail(email);
+                userLoginRequest.setPassword(password);
 
-        //Check for network availability
-        if(isNetworkAvailable()){
-            //If available, Perform Login Request
+                //Build the service from your API Interface
+                HHApiClient service = retrofit.create(HHApiClient.class);
+                //Call your service with Retrofit: Call<your expected response class> = service.[whatever_you_named_your_endpoint_in_the_interface]
+                Call<UserLoginResponse> userLoginResponseCall =  service.signIn(userLoginRequest);
+                userLoginResponseCall.enqueue(new Callback<UserLoginResponse>() {
+                    @Override
+                    public void onResponse(Call<UserLoginResponse> call, Response<UserLoginResponse> response) {
 
-            //Start building a retrofit object.
-            Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl(URL_BASE)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
+                        int statusCode = response.code();
+                        UserLoginResponse userLoginResponse = response.body();
+                        String name = userLoginResponse.getUsers().getFirstName();
+                        Log.v(TAG, "RESPONSE RECEIVED----> " + statusCode);
+                        Log.v(TAG, "RESPONSE RECEIVED----> " + name);
 
-            //Generate a login from the input data
-            UserLoginRequest userLoginRequest = new UserLoginRequest();
-            userLoginRequest.setEmail(email);
-            userLoginRequest.setPassword(password);
+                        Intent userIntent = new Intent(LoginActivity.this, UserActivity.class);
+                        userIntent.putExtra("username", userLoginResponse.getUsers().getEmail());
+                        userIntent.putExtra("phoneNumber", userLoginResponse.getUsers().getPhoneNumber());
+                        userIntent.putExtra("users", serializeBooty(userLoginResponse));
+                        LoginActivity.this.startActivity(userIntent);
+                        finish();
+                    }
 
-            //Build the service from your API Interface
-            HHApiClient service = retrofit.create(HHApiClient.class);
-            //Call your service with Retrofit: Call<your expected response class> = service.[whatever_you_named_your_endpoint_in_the_interface]
-            Call<UserLoginResponse> userLoginResponseCall =  service.signIn(userLoginRequest);
-
-            userLoginResponseCall.enqueue(new Callback<UserLoginResponse>() {
-                @Override
-                public void onResponse(Call<UserLoginResponse> call, Response<UserLoginResponse> response) {
-
-                    int statusCode = response.code();
-                    UserLoginResponse userLoginResponse = response.body();
-                    String name = userLoginResponse.getUsers().getFirstName();
-                    Log.v(TAG, "RESPONSE RECEIVED----> " + statusCode);
-                    Log.v(TAG, "RESPONSE RECEIVED----> " + name);
-
-                }
-
-                @Override
-                public void onFailure(Call<UserLoginResponse> call, Throwable t) {
-                    Log.v(TAG, "FAILURE  ----> " + t.getMessage());
-                }
-            });
-        }else{
-            //If not available, Toast a message to the user indicating lack of network availability
-            Toast.makeText(this, "NETWORK IS UNAVAILABLE", Toast.LENGTH_LONG).show();
+                    @Override
+                    public void onFailure(Call<UserLoginResponse> call, Throwable t) {
+                        Log.v(TAG, "FAILURE  ----> " + t.getMessage());
+                    }
+                });
+            }else{
+                //If not available, Toast a message to the user indicating lack of network availability
+                Toast.makeText(this, "NETWORK IS UNAVAILABLE", Toast.LENGTH_LONG).show();
+            }
         }
+
+
+
 
         //Catch errors
     }
 
     private boolean isNetworkAvailable() {
+
         ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = manager.getActiveNetworkInfo();
 
@@ -141,4 +153,21 @@ public class LoginActivity extends AppCompatActivity {
         }
         return isAvailable;
     }
+
+    private boolean isEmpty (EditText checkText){
+        Log.v(TAG, "Checking Length for text in field " );
+        if(checkText.getText().toString().trim().length() == 0){
+            Log.v(TAG, "EMPTY FIELD" );
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public String serializeBooty(UserLoginResponse userLoginResponse){
+        Gson gson = new Gson();
+        String s = gson.toJson(userLoginResponse);
+        return s;
+    }
+
 }
--- app/src/main/java/com/lukeaskins/logindemo/ui/UserActivity.java
+++ app/src/main/java/com/lukeaskins/logindemo/ui/UserActivity.java
@@ -1,25 +1,46 @@
 package com.lukeaskins.logindemo.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
 import com.lukeaskins.logindemo.R;
+import com.lukeaskins.logindemo.model.UserLoginResponse;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class UserActivity extends AppCompatActivity {
-    @BindView(R.id.etUsername) EditText etUsername;
-    @BindView(R.id.etAge) EditText etAge;
+    @BindView(R.id.userName) TextView userName;
+    @BindView(R.id.phoneNumber) TextView phoneNumber;
+    String mUsername;
+    String mPhoneNumber;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.acitivy_user);
+        setContentView(R.layout.activity_user);
         ButterKnife.bind(this);
+        Intent intent = getIntent();
 
+        UserLoginResponse deSerializedData = deSerializeBooty(intent.getStringExtra("users"));
+
+        mUsername = deSerializedData.getUsers().getEmail();
+        mPhoneNumber = deSerializedData.getUsers().getFirstName();
+        userName.setText(mUsername);
+        phoneNumber.setText(mPhoneNumber);
     }
+
+    public UserLoginResponse deSerializeBooty(String s){
+        Gson gson = new Gson();
+        UserLoginResponse deSerializedData = gson.fromJson(s, UserLoginResponse.class);
+
+        return deSerializedData;
+    }
+
 }
